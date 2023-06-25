6db0807ac6863b03bc3e271662ad275af0881106
Artem Yakimenko <artem.yakimenko@gmail.com>
2023-06-25 13:55:42 +0300

    - added custom flyway migration for r2dbc

---
+++ src/main/kotlin/com/example/simplechat/config/DatabaseConfig.kt
@@ -0,0 +1,25 @@
+package com.example.simplechat.config
+
+import org.flywaydb.core.Flyway
+import org.springframework.boot.autoconfigure.flyway.FlywayProperties
+import org.springframework.boot.autoconfigure.r2dbc.R2dbcProperties
+import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@EnableConfigurationProperties(value = [R2dbcProperties::class, FlywayProperties::class])
+class DatabaseConfig {
+    @Bean(initMethod = "migrate")
+    fun flyway(flywayProperties: FlywayProperties, r2dbcProperties: R2dbcProperties): Flyway {
+        return Flyway.configure()
+            .dataSource(
+                flywayProperties.url,
+                r2dbcProperties.username,
+                r2dbcProperties.password
+            )
+            .locations(*flywayProperties.locations.toTypedArray())
+            .baselineOnMigrate(true)
+            .load()
+    }
+}
